846957749e040aedde742258c2caa318e5b4b1b6
elabbas0 <arkanbak09@gmail.com>
2026-01-25 02:37:01 +0400

    visit entity

---
+++ Models/Entities/Visit.cs
@@ -0,0 +1,17 @@
+﻿namespace EmployeeTrack.Models.Entities
+{
+    public class Visit
+    {   
+        public int Id { get; set; }
+        public int employeeId { get; set; }
+        public Employee? employee { get; set; }
+
+        public int countryId { get; set; }
+        public Country? country { get; set; }
+
+        public Category? category { get; set; }
+
+        public DateOnly startDate { get; set; }
+        public DateOnly? endDate { get; set; }
+    }
+}
