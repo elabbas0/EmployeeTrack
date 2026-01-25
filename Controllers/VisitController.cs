f28962fca734642c717afa4e4ed4d7a7632530a2
elabbas0 <arkanbak09@gmail.com>
2026-01-25 07:09:25 +0400

    visit controller implemention similar to employee

--- Controllers/VisitController.cs
+++ Controllers/VisitController.cs
@@ -1,6 +1,50 @@
-﻿namespace EmployeeTrack.Controllers
+﻿using EmployeeTrack.Models.Entities;
+using EmployeeTrack.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeTrack.Controllers
 {
-    public class VisitController
+    public class VisitController : Controller
     {
+        private readonly AppDbContext _db;
+        public VisitController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpPost]
+        public IActionResult Create(Visit visit)
+        {
+            _db.Visits.Add(visit);
+            _db.SaveChanges();
+
+            if (visit.Id == 0)
+            {
+                return BadRequest("Failed to create visit record.");
+            }
+
+            return Ok(visit.Id);
+        }
+
+        public Visit? GetById(int id)
+        {
+            return _db.Visits.Find(id);
+        }
+
+
+        [HttpPost]
+        public IActionResult DeleteById(int id)
+        {
+            var visit = _db.Visits.Find(id);
+
+            if (visit != null)
+            {
+                _db.Visits.Remove(visit);
+                _db.SaveChanges();
+                return Ok($"Visit ID {id} deleted successfully.");
+            }
+
+            return NotFound($"Visit with ID {id} not found.");
+        }
     }
 }
